e63d01bc752cded2c0ae8da178fb21224eccb8b6
zhang.xiaokang <zhangkangen@gmail.com>
2015-12-05 19:39:14 +0800

    学会了mvc中js和css的载入方式

--- MvcLucene/App_Start/BundleConfig.cs
+++ MvcLucene/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace MvcLucene
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
@@ -24,6 +27,9 @@ namespace MvcLucene
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
